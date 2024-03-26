8453d63d55d87f5358715e5fe5316641ecab28a4
NicoBorsdorf <nico-borsdorf@gmx.de>
2024-03-26 21:33:18 +0100

    dropbox page

---
+++ Controllers/DropboxController.cs
@@ -0,0 +1,25 @@
+
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiDa.Controllers
+{
+    public class DropboxController : Controller
+    {
+        // 
+        // GET: /PCloud/ 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
