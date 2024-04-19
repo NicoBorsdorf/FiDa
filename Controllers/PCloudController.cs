f612615ee6dcc88ae20f0f179eb8e4c27e98875a
nico.borsdorf <nico.borsdorf@siemens.com>
2024-04-19 16:11:50 +0200

    file upload

--- Controllers/PCloudController.cs
+++ Controllers/PCloudController.cs
@@ -1,20 +1,29 @@
 using FiDa.Database;
 using FiDa.Models;
 using Microsoft.AspNetCore.Mvc;
-using pcloud_sdk_csharp.Controllers;
 using pcloud_sdk_csharp.Requests;
+using pcloud_sdk_csharp.Responses;
+using System.Text.Json;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
 namespace FiDa.Controllers
 {
     public class PCloudController : Controller
     {
-        private readonly FiDaDatabase db = new();
+        private readonly FiDaDatabase Db = new();
+        private readonly IConfiguration _Config;
+
+        public PCloudController(IConfiguration configuration)
+        {
+            _Config = configuration;
+        }
 
         // 
         // GET: /pcloud/ 
         public ActionResult Index()
         {
-            List<FileUpload> files = db.UploadedFiles.ToList();
+            List<FileUpload> files = Db.UploadedFiles.ToList();
             return View(files);
         }
 
@@ -26,25 +35,53 @@ namespace FiDa.Controllers
 
             foreach (var file in files)
             {
-                Console.WriteLine("file " + file.FileName);
-                //try
-                //{
-                StreamReader _file = new(file.OpenReadStream());
-                UploadFileRequest req = new(folderId, file.FileName, _file);
-                var res = FileController.UploadFile(req, "64qbZMEJjKgdvE57ZUI7t7kZCApm81pUrChgxtJQwE7BBHjgQbJV");
-                res.Wait();
+                try
+                {
+                    Stream _file = file.OpenReadStream();
+                    Console.WriteLine("folderid " + folderId);
+                    Console.WriteLine("_file.Length " + _file.Length);
+                    Console.WriteLine("file.Name " + file.FileName);
+                    UploadFileRequest req = new(folderId, file.FileName, _file);
+                    var res = FileController.UploadFile(req, _Config["API_Tokens:PCloud"]).Result;
 
-                Console.WriteLine(res.Result);
-                ViewBag.Message = "File Uploaded Successfully!!";
-                //}
-                //catch (Exception e)
-                //{
-                //    Console.WriteLine($"Exception for {file.FileName} occured: {e.Message}");
-                //    ViewBag.Message += $"File upload failed for file: {file.FileName} <br />";
-                //}
+                    Console.WriteLine(res.ToString());
+                    Console.WriteLine(res.fileids.Count());
+                    Console.WriteLine(res.metadata.Count());
+                    ViewBag.Message = "File Uploaded Successfully!!";
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine($"Exception for {file.FileName} occured: {e.Message}");
+                    //ViewBag.Message += $"File upload failed for file: {file.FileName} <br />";
+                }
             }
 
             return View("Index");
+        }
+
+        public class FileController
+        {
+            private static readonly string baseURL = "https://eapi.pcloud.com/";
+            private static readonly HttpClient client = new();
+
+            public async static Task<UploadedFile?> UploadFile(UploadFileRequest req, string token)
+            {
+                var formData = new MultipartFormDataContent();
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                formData.Add(new StringContent(req.FolderId.ToString()), "folderid");
+                formData.Add(new ByteArrayContent(req.UploadFile), "file");
+                formData.Add(new StringContent(req.FileName), "filename");
+
+                Console.WriteLine(formData.ToString());
+                Console.WriteLine("formdata " + JsonSerializer.Serialize(formData));
+
+                HttpResponseMessage response = await client.PostAsync(baseURL + "uploadfile", formData);
+
+                return JsonSerializer.Deserialize<UploadedFile>(await response.Content.ReadAsStringAsync());
+            }
         }
     }
 }
