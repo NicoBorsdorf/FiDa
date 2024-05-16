9fc07e0f1b5c79d11de153062789ab242a281d3e
nico.borsdorf <nico.borsdorf@siemens.com>
2024-05-16 15:51:32 +0200

    updated view model

--- Models/FileViewModel.cs
+++ Models/FileViewModel.cs
@@ -4,29 +4,14 @@ namespace FiDa.ViewModels
 {
     public class FileViewModel
     {
-        public string Id { get; set; }
-
-        public string FileName { get; set; }
-
-        public Hosts Hostname { get; set; }
-
-        public double? Size { get; set; }
-
-        public long ParentFolderId { get; set; }
-
-        public bool IsFolder { get; set; } = false;
-
-        public long FileId { get; set; }
-
-        public DateTime ModificationDate { get; set; } = DateTime.Now;
-
-        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public List<UploadedFile> RootFiles { get; set; }
+        public List<UploadedFile> Folders { get; set; }
     }
 
     public class UserViewModel
     {
         public string UserName { get; set; }
-        public string Email { get; set; }
-        public string ProfilePicture { get; set; }
+        public string? Email { get; set; }
+        public string? ProfilePicture { get; set; }
     }
 }
