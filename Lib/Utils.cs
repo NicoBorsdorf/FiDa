a0aaac689cdecfe982f945e1b9ea3b428b446fc0
nico.borsdorf <nico.borsdorf@siemens.com>
2024-05-16 15:51:23 +0200

    added lib

---
+++ Lib/Utils.cs
@@ -0,0 +1,24 @@
+﻿using FiDa.Database;
+using FiDa.DatabaseModels;
+
+namespace FiDa.Lib
+{
+    public static class Utils
+    {
+        private static readonly FiDaDatabase _db = new();
+        public static async Task<Account?> AddAccount(Account account)
+        {
+            try
+            {
+                await _db.AddAsync(account);
+                await _db.SaveChangesAsync();
+                return account;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
