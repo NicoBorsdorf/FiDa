8760bf458b1a37461d550d4044f0555cf6566304
nico.borsdorf <nico.borsdorf@siemens.com>
2024-05-17 16:11:30 +0200

    nulls

--- Models/FileViewModel.cs
+++ Models/FileViewModel.cs
@@ -4,13 +4,13 @@ namespace FiDa.ViewModels
 {
     public class FileViewModel
     {
-        public List<UploadedFile> RootFiles { get; set; }
-        public List<UploadedFile> Folders { get; set; }
+        public List<UploadedFile> RootFiles { get; set; } = new List<UploadedFile>();
+        public List<UploadedFile> Folders { get; set; } = new List<UploadedFile>();
     }
 
     public class UserViewModel
     {
-        public string UserName { get; set; }
+        public string UserName { get; set; } = null!;
         public string? Email { get; set; }
         public string? ProfilePicture { get; set; }
     }
