795298997d40c0ffce0a8b3ed7b444724ac7fc08
NicoBorsdorf <nico-borsdorf@gmx.de>
2024-05-19 17:18:25 +0200

    db connection with docker

--- Database/DatabaseContext.cs
+++ Database/DatabaseContext.cs
@@ -1,3 +1,4 @@
+using System.Xml;
 using FiDa.DatabaseModels;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,7 +18,8 @@ namespace FiDa.Database
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=true;MultipleActiveResultSets=true");
+                //optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=true;MultipleActiveResultSets=true");
+                optionsBuilder.UseSqlServer("Data Source=sql_server, 1433;Initial Catalog=FiDaDatabase;User Id=sa;Password=A&VeryComplex123Password;Integrated Security=false;TrustServerCertificate=true;");
             }
         }
 
