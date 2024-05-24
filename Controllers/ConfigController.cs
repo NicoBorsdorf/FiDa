b06ed943a1c295e9b1f2f6428286dfb618649a3f
nico.borsdorf <nico.borsdorf@siemens.com>
2024-05-24 09:21:27 +0200

    redirect

--- Controllers/ConfigController.cs
+++ Controllers/ConfigController.cs
@@ -25,11 +25,13 @@ namespace FiDa.Controllers
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult PCloud_Authenticate(IFormCollection formData)
         {
             string client_id = formData["client_id"]!;
             string client_secret = formData["client_secret"]!;
+            TempData["client_id"] = client_id;
+            TempData["client_secret"] = client_secret;
 
             var baseUri = $"{Request.Scheme}://{Request.Host}/config/pcloud_authenticate_redirect";
             var authReq = new AuthorizeRequest(client_id, AuthorizeRequest.ResponseType.code, client_secret, baseUri);
@@ -43,16 +45,29 @@ namespace FiDa.Controllers
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult PCloud_Authenticate_Rdirect()
+        [Authorize]
+        public ActionResult PCloud_Authenticate_Redirect()
         {
             var query = Url.ActionContext.HttpContext.Request.Query.ToDictionary(x => x.Key, x => x.Value);
             var code = query.GetValueOrDefault("code").ToString();
-            if (null == code) throw new ArgumentNullException("No code found in query");
+            if (null == code) throw new ArgumentNullException(nameof(code));
             //var location = query.GetValueOrDefault("location");
 
-            var (access_token) = AuthController.GetAccessToken(new { client_id = });
+            var access_token = AuthController.GetOAuth_Token((string)TempData["client_id"], (string)TempData["client_secret"], code)?.Result?.access_token;
 
+            if (null == access_token) throw new ArgumentNullException(nameof(access_token));
+
+            _currentUser.ConfiguredHosts.Add(new UserHost
+            {
+                Account = _currentUser,
+                AccountId = _currentUser.Id,
+                ApiKey = access_token,
+                Host = Hosts.PCloud
+            });
+            _db.Account.Update(_currentUser);
+            _db.SaveChangesAsync().Wait();
+
+            return RedirectToActionPermanent("Index", "PCloud");
         }
     }
 }
