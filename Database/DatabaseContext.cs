456ac7db9011e5a4b53883111282dcce2307e3ef
nico.borsdorf <nico.borsdorf@siemens.com>
2024-07-01 10:01:37 +0200

    db update

--- Database/DatabaseContext.cs
+++ Database/DatabaseContext.cs
@@ -1,4 +1,3 @@
-using System.Xml;
 using FiDa.DatabaseModels;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,10 +6,6 @@ namespace FiDa.Database
     public class FiDaDatabase : DbContext
     {
         public FiDaDatabase(DbContextOptions<FiDaDatabase> options) : base(options)
-        {
-        }
-
-        public FiDaDatabase()
         {
         }
 
@@ -23,36 +18,78 @@ namespace FiDa.Database
             }
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<UploadedFile>()
-                    .Property(s => s.Modified)
-                    .HasDefaultValueSql("GETDATE()");
-            modelBuilder.Entity<UploadedFile>()
-                   .Property(s => s.Created)
-                   .HasDefaultValueSql("GETDATE()");
-
-            modelBuilder.Entity<Account>()
-                    .Property(s => s.Modified)
-                    .HasDefaultValueSql("GETDATE()");
-            modelBuilder.Entity<Account>()
-                    .Property(s => s.Created)
-                    .HasDefaultValueSql("GETDATE()");
-
-            modelBuilder.Entity<UserHost>()
-                   .Property(s => s.Modified)
-                   .HasDefaultValueSql("GETDATE()");
-            modelBuilder.Entity<UserHost>()
-                  .Property(s => s.Created)
-                  .HasDefaultValueSql("GETDATE()");
-        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<UploadedFile>(entity =>
+        //    {
+        //        entity.HasKey(f => f.Id);
+        //        entity.HasIndex(f => f.Id).IsUnique();
+        //        entity.Property(f => f.Id).ValueGeneratedOnAdd();
+        //
+        //        entity.Property(f => f.FileName).IsRequired().HasMaxLength(255);
+        //
+        //        entity.Property(f => f.FileId).IsRequired();
+        //
+        //        entity.Property(f => f.ParentFolderId).IsRequired();
+        //
+        //        entity.Property(f => f.Size).HasPrecision(18, 2);
+        //
+        //        entity.HasOne(f => f.Host).WithMany(h => h.Files).HasForeignKey(f => f.HostId).OnDelete(DeleteBehavior.Restrict);
+        //
+        //        entity.HasOne(f => f.Account).WithMany(a => a.Files).HasForeignKey(e => e.AccountId).OnDelete(DeleteBehavior.Restrict);
+        //
+        //        entity.Property(f => f.Modified).ValueGeneratedOnAddOrUpdate().HasDefaultValueSql("GETDATE()");
+        //
+        //        entity.Property(f => f.Created).ValueGeneratedOnAdd().HasDefaultValueSql("GETDATE()");
+        //    });
+        //
+        //    modelBuilder.Entity<Account>(entity =>
+        //    {
+        //        entity.HasKey(a => a.Id);
+        //        entity.HasIndex(a => new { a.Id, a.Username }).IsUnique();
+        //        entity.Property(a => a.Id).ValueGeneratedOnAdd();
+        //
+        //        entity.Property(a => a.Username).IsRequired();
+        //
+        //        entity.Property(a => a.Username).IsRequired().HasMaxLength(255);
+        //
+        //        entity.HasMany(a => a.Files).WithOne(a => a.Account).HasForeignKey(f => f.AccountId).OnDelete(DeleteBehavior.Cascade);
+        //
+        //        entity.HasMany(a => a.ConfiguredHosts).WithOne(a => a.Account).HasForeignKey(u => u.AccountId).OnDelete(DeleteBehavior.Cascade);
+        //
+        //        entity.Property(a => a.Modified).ValueGeneratedOnAddOrUpdate().HasDefaultValueSql("GETDATE()");
+        //
+        //        entity.Property(a => a.Created).ValueGeneratedOnAdd().HasDefaultValueSql("GETDATE()");
+        //    });
+        //
+        //    modelBuilder.Entity<UserHost>(entity =>
+        //    {
+        //        entity.HasKey(u => u.Id);
+        //        entity.HasIndex(u => u.Id).IsUnique();
+        //        entity.Property(u => u.Id).ValueGeneratedOnAdd();
+        //
+        //        entity.Property(u => u.ApiKey).IsRequired();
+        //
+        //        entity.Property(u => u.Host).IsRequired().HasConversion<int>();
+        //
+        //        entity.Ignore(u => u.HostAddress);
+        //
+        //        entity.Property<string>("_uriString").HasColumnName("UriString");
+        //
+        //        entity.HasOne(a => a.Account).WithMany(a => a.ConfiguredHosts).HasForeignKey(u => u.AccountId).IsRequired().OnDelete(DeleteBehavior.Restrict);
+        //
+        //        entity.HasMany(u => u.Files).WithOne(f => f.Host).HasForeignKey(f => f.HostId).OnDelete(DeleteBehavior.Cascade);
+        //
+        //        entity.Property(u => u.Modified).ValueGeneratedOnAddOrUpdate().HasDefaultValueSql("GETDATE()");
+        //
+        //        entity.Property(u => u.Created).ValueGeneratedOnAdd().HasDefaultValueSql("GETDATE()");
+        //        entity.Property(e => e.Str);
+        //    });
+        //}
 
         // Db sets
         public DbSet<UploadedFile> UploadedFiles { get; set; }
         public DbSet<Account> Account { get; set; }
         public DbSet<UserHost> UserHost { get; set; }
-
     }
-
-
 }
--- Database/DatabaseTables.cs
+++ Database/DatabaseTables.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FiDa.Database;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -13,9 +14,12 @@ namespace FiDa.DatabaseModels
 
         [Required(ErrorMessage = "FileName is required")]
         [StringLength(255, MinimumLength = 1, ErrorMessage = "FileName must be between 1 and 255 characters")]
-        public string FileName { get; set; } = null!;
+        public string FileName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Host is required")]
+        public int HostId { get; set; }
+        [ForeignKey(nameof(HostId))]
+        [DeleteBehavior(DeleteBehavior.NoAction)]
         public UserHost Host { get; set; } = null!;
 
         [Range(0, double.MaxValue, ErrorMessage = "Size must be a positive number")]
@@ -23,23 +27,28 @@ namespace FiDa.DatabaseModels
         public decimal? Size { get; set; }
 
         [Required(ErrorMessage = "A Parent Folder id is required.")]
-        public string ParentFolderId { get; set; } = null!;
+        [StringLength(255, MinimumLength = 1, ErrorMessage = "A Parent Folder id is required.")]
+        public string ParentFolderId { get; set; } = string.Empty;
 
         public bool IsFolder { get; set; } = false;
 
         [Required(ErrorMessage = "A File Id is required.")]
-        public string FileId { get; set; } = null!;
+        [StringLength(255, MinimumLength = 1, ErrorMessage = "A File Id is required.")]
+        public string FileId { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "File can not be created without an set Account")]
+        public int AccountId { get; set; } // Foreign key property
+        [ForeignKey(nameof(AccountId))]
+        [DeleteBehavior(DeleteBehavior.NoAction)]
         public Account Account { get; set; } = null!;
 
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy HH:mm}")]
         [DataType(DataType.Date)]
-        public DateTime Modified { get; set; } = DateTime.Now;
+        public DateTime Modified { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy HH:mm}")]
         [DataType(DataType.Date)]
-        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Created { get; set; }
     }
 
     [Index(nameof(Id), nameof(Username), IsUnique = true)]
@@ -48,7 +57,6 @@ namespace FiDa.DatabaseModels
         public Account(string username)
         {
             Username = username;
-            ConfiguredHosts = new List<UserHost>();
         }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -56,16 +64,19 @@ namespace FiDa.DatabaseModels
         public int Id { get; }
 
         [Required(ErrorMessage = "Username is required.")]
-        public string Username { get; private set; }
+        public string Username { get; set; }
 
-        [ForeignKey("AccountId")]
-        public ICollection<UserHost> ConfiguredHosts { get; private set; }
+        public ICollection<UserHost> ConfiguredHosts { get; } = new List<UserHost>();
+
+        public ICollection<UploadedFile> Files { get; } = new List<UploadedFile>();
 
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public DateTime Modified { get; private set; }
+        [DataType(DataType.Date)]
+        public DateTime Modified { get; }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public DateTime Created { get; private set; }
+        [DataType(DataType.Date)]
+        public DateTime Created { get; }
     }
 
     [Index(nameof(Id), IsUnique = true)]
@@ -77,23 +88,35 @@ namespace FiDa.DatabaseModels
 
         [Required(ErrorMessage = "A Host Name is required.")]
         public Hosts Host { get; set; }
-        public ICollection<UploadedFile> Files { get; private set; } = new List<UploadedFile>();
+        public ICollection<UploadedFile> Files { get; } = new List<UploadedFile>();
 
         [Required(ErrorMessage = "A Api key must be provided.")]
-        public string ApiKey { get; set; } = null!;
+        public string ApiKey { get; set; } = string.Empty;
+
+        [NotMapped]
+        public Uri? HostAddress
+        {
+            get => _uriString != null ? new(_uriString) : null;
+            set => _uriString = value?.ToString();
+        }
+
+        [Column("UriString")]
+        private string? _uriString;
 
         [Required(ErrorMessage = "Account is required")]
-        public int AccountId { get; set; }
-        [Required(ErrorMessage = "Account is required")]
-        public virtual Account Account { get; set; } = null!;
+        public int AccountId { get; set; } // Foreign key property
+        [ForeignKey(nameof(AccountId))]
+        [DeleteBehavior(DeleteBehavior.NoAction)]
+        public Account Account { get; set; } = null!;
 
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [DataType(DataType.Date)]
         public DateTime Modified { get; }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DataType(DataType.Date)]
         public DateTime Created { get; }
     }
-
 
     public enum Hosts
     {
