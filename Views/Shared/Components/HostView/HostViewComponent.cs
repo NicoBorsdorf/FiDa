6fbe2a0f50ae8f2a0edaf1247aa0478d1e01598e
NicoBorsdorf <nico-borsdorf@gmx.de>
2024-07-23 14:44:57 +0200

    centralized host view

---
+++ Views/Shared/Components/HostView/HostViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using FiDa.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiDa.Views.Shared.Components.HostView
+{
+    [ViewComponent(Name = "HostView")]
+    public class HostViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(BaseViewModel model) => await Task.FromResult<IViewComponentResult>(View("HostView", model));
+    }
+}
